54d79bde57623896dbf8ab244bd7e8c61f0bceff
hardik-tamore <hardiktamore.ht@gmail.com>
2021-09-11 23:09:57 +0530

    image

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,7 +8,7 @@ function Card({title, size, price, id, copy_text, photo}) {
     <div className="card">
       <div className="card-image">
           <FileCopyIcon id = 'copy-btn' onClick={() => {navigator.clipboard.writeText(copy_text)}} />
-        <img src={`https://cryptic-lowlands-74955.herokuapp.com/uploads/285ec4fa-626e-41b2-abb6-1da25c7de6ec-1631365587642.JPG`} />
+        <img src={`https://cryptic-lowlands-74955.herokuapp.com/uploads/${photo}`} />
       </div>
       <div className="card-info">
         <h5>{title+' ('+size+')'}</h5>
